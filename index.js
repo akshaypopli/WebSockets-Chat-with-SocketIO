3c0513ff15b492505614a46fbce48983b02b4910
Mac <mac@Akshays-Air.local>
2020-11-13 22:37:11 -0500

    changes committed

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ var http= require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/',(req,res) =>{
-    //res.send('<h1> Hello World </h1>');
     res.sendFile(__dirname + '/index.html');
 });
 
